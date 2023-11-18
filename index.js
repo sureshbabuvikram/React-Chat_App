e7ee843121a296df6ef3bc883ef173a714c54866
sureshbabuvikram <sureshbabuvikram@gmail.com>
2023-11-19 00:37:12 +0530

    complete

--- index.js
+++ index.js
@@ -11,24 +11,24 @@ app.use(cors());
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    // try {
-    //     const result = await axios.put("https://api.chatengine.io/users/",
-    //         {
-    //             "username": username,
-    //             "secret": username,
-    //             "first_name":username
-    //         },
-    //         {
-    //             headers:{"private-key":"354cde1a-7bf8-475c-95d0-8b8ce96917d7"}
-    //         })
-    //     return res.status(result.status).json(result.data)
-    // } catch (e) {
-    //     return res.status(e.response.status).json(e.response.data)
+    try {
+        const result = await axios.put("https://api.chatengine.io/users/",
+            {
+                "username": username,
+                "secret": username,
+                "first_name":username
+            },
+            {
+                headers:{"private-key":"354cde1a-7bf8-475c-95d0-8b8ce96917d7"}
+            })
+        return res.status(result.status).json(result.data)
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data)
 
-    // }
+    }
 
 
-    return res.json({ username: username, secret: "sha256..." });
+    // return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
